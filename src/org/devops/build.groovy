a28e999df338cc7fde0b936931fde89331bd0cf0
spider <spider@duxiao.me>
2020-11-29 21:22:07 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,8 +6,9 @@ def Build(buildType,buildShell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"Gradle","npm":"NPM"]
     println("当前选择的类型为 ${buildType}")
     buildHome = tool buildTools[buildType]
+    
     if ("${buildType == "npm"}"){
-        npmHome = tool "NodeJS"
+        //npmHome = tool "NodeJS"
         sh "export NODE_HOME=/usr/local/node-v14.15.1-linux-x64 && export PATH=\$NODE_HOME/bin:\$PATH  && ${npmHome}/bin/npm ${buildShell}"
     }else {
         sh "${buildHome}/bin/${buildType} ${buildShell}"
